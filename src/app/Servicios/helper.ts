27e434acb9e8774b6d4743e0dcbb840051279cfc
Santalb <143513988+Santalb@users.noreply.github.com>
2024-07-02 16:27:12 -0500

    V03

--- src/app/Servicios/helper.ts
+++ src/app/Servicios/helper.ts
@@ -1,2 +1,2 @@
-let baserUrl = 'http://localhost:8081'
+let baserUrl = 'postgresql://alexis:zxEixOTIsXMWkEFBC8I8npgLGa4UnDFD@dpg-cq25qmbv2p9s73en50b0-a.ohio-postgres.render.com/rentease_rfua'
 export default baserUrl;
