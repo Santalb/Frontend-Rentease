1b06e2afd1d556b00f3d9e46a7e6ca7df423a14b
Santalb <143513988+Santalb@users.noreply.github.com>
2024-07-02 18:16:14 -0500

    Funcionooooo

--- src/app/Servicios/helper.ts
+++ src/app/Servicios/helper.ts
@@ -1,2 +1,2 @@
-let baserUrl = 'postgresql://dpg-cq25qmbv2p9s73en50b0-a.ohio-postgres.render.com:5432/rentease_rfua'
+let baserUrl = 'https://backend-rentease.onrender.com'
 export default baserUrl;
